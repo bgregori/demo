360c17b27cc8f3f66eb932fd94b7944661d326d9
Bryan Gregorius <bgregori@redhat.com>
2020-08-12 09:47:08 -0400

    Add metrics

--- src/main/java/org/acme/demo/GreetingResource.java
+++ src/main/java/org/acme/demo/GreetingResource.java
@@ -5,11 +5,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+
+
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Counted(name = "greetingsCount", description = "How many times hello endpoint was invoked")
     public String hello() {
         return "hello";
     }
